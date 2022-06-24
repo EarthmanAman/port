895f7b377d6e14afd87fdc023b55f3d8b8b5515a
EarthmanAman <hashimathman@gmail.com>
2022-06-24 15:43:24 +0300

    Home page

--- components/home/nav.js
+++ components/home/nav.js
@@ -1,34 +1,35 @@
 import Image from 'next/image'
-import { FaTwitter, FaGithub, FaLinkedin, FaFacebookF } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function NavBar() {
     return (
-        <div className='w-full flex justify-between items-center'>
-            <div className='flex-1 w-full'>
+        <div className='flex justify-between items-center'>
+            <div className='w-full'>
                 <ul className='flex items-center'>
+
                     <li className='mr-10 text-xs font-bold'>WORKS</li>
                     <li className='mr-10 text-xs font-bold'>CONTACT</li>
                     <li className='mr-10 text-xs font-bold'>RESUME</li>
                 </ul>
             </div>
 
-            <div className='flex-1 w-full'>
+            <div className='w-full flex justify-center'>
                 <Image src={"/../public/assets/logo.ico"} width="50" height="50" alt="logo" />
             </div>
 
-            <div className='flex-1 w-full'>
-                <div className='flex'>
-                    <div className='ml-3'>
-                        <FaGithub size={15} />
+            <div className='w-full'>
+                <div className='flex justify-end'>
+                    <div className='ml-4'>
+                        <FaGithub size={15} color="#1a1e22" />
                     </div>
-                    <div className='ml-3'>
-                        <FaLinkedin size={15} />
+                    <div className='ml-4'>
+                        <FaLinkedin size={15} color="#2764ac" />
                     </div>
-                    <div className='ml-3'>
-                        <FaTwitter size={15} />
+                    <div className='ml-4'>
+                        <FaTwitter size={15} color="#1c96e8" />
                     </div>
-                    <div className='ml-3'>
-                        <FaFacebookF size={15} />
+                    <div className='ml-4'>
+                        <FaInstagram size={15} color="#f60961" />
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "PlayFair": ["PlayFair Display"],
+        "DancingScript": ["Dancing Script"],
+        "Montserrat": ["Montserrat"]
+      },
+      height: {
+        85: '22rem',
+      },
+    },
   },
   plugins: [],
 }
